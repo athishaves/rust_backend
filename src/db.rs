8b739067b0ee896ac4a97177b352da0abcd52723
Athish Venkatesh <athishaves@gmail.com>
2024-02-10 21:45:41 +0530

    refractor to onion architecture

--- src/db.rs
+++ src/db.rs
@@ -1,3 +1,3 @@
 pub mod database;
 pub use database::Database;
-pub mod pizza_data_trait;
+pub mod pizza_data;
---
+++ src/web/pizza_web.rs
@@ -0,0 +1,56 @@
+use actix_web::{get, post, patch};
+use actix_web::web::{Data, Json, Path};
+use uuid::Uuid;
+use validator::Validate;
+
+use crate::db::{pizza_data::PizzaDataTrait, Database};
+use crate::error::PizzaError;
+use crate::models::{BuyPizzaRequest, Pizza, UpdatePizzaUrl};
+
+#[get("/pizzas")]
+async fn get_pizzas(db: Data<Database>) -> Result<Json<Vec<Pizza>>, PizzaError> {
+  let pizzas = Database::get_all_pizzas(&db).await;
+  match pizzas {
+    Some(found_pizzas) => Ok(Json(found_pizzas)),
+    None => Err(PizzaError::NoPizzasFound),
+  }
+}
+
+#[post("/buypizza")]
+async fn buy_pizza(
+  body: Json<BuyPizzaRequest>,
+  db: Data<Database>,
+) -> Result<Json<Pizza>, PizzaError> {
+  let is_valid = body.validate();
+  match is_valid {
+    Ok(_) => {
+      let pizza_name = body.pizza_name.clone();
+
+      let mut buffer = Uuid::encode_buffer();
+      let new_uuid = Uuid::new_v4().simple().encode_lower(&mut buffer);
+
+      let new_pizza =
+        Database::add_pizza(&db, Pizza::new(String::from(new_uuid), pizza_name)).await;
+
+      match new_pizza {
+        Some(created) => Ok(Json(created)),
+        None => Err(PizzaError::PizzaCreationFailure),
+      }
+    }
+    Err(_) => Err(PizzaError::InvalidUserInput),
+  }
+}
+
+#[patch("/updatepizza/{uuid}")]
+async fn update_pizza(
+  update_pizza_url: Path<UpdatePizzaUrl>,
+  db: Data<Database>,
+) -> Result<Json<Pizza>, PizzaError> {
+  let uuid = update_pizza_url.into_inner().uuid;
+  let updated_pizza = Database::update_pizza(&db, uuid).await;
+
+  match updated_pizza {
+    Some(updated) => Ok(Json(updated)),
+    None => Err(PizzaError::NoSuchPizzaFound),
+  }
+}
